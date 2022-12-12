48e787fea3808c8fb6750f4c8cafeafefabe7996
U1 <fred.frog75@gmail.com>
2022-12-12 06:39:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dampopup/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dampopup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NoticeDialogFragment.NoticeDialogListener {
+    private Button btnFragment;
+    private NoticeDialogFragment noticeDialogFragment;
+
+
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogBtn1Click(NoticeDialogFragment dialog) {
+        // User touched the dialog's positive button
+        Toast.makeText(this, "BOUTON 1 ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDialogBtn2Click(NoticeDialogFragment dialog) {
+        // User touched the dialog's negative button
+        Toast.makeText(this, "BOUTON 2 ", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFragment=findViewById(R.id.btnFragment);
+        btnFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noticeDialogFragment =new NoticeDialogFragment();
+                noticeDialogFragment.show(getSupportFragmentManager(),"My  Fragment");
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/dampopup/NoticeDialogFragment.java
@@ -0,0 +1,74 @@
+package com.example.dampopup;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class NoticeDialogFragment extends androidx.fragment.app.DialogFragment {
+
+    private Button btn1, btn2;
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface NoticeDialogListener {
+        public void onDialogBtn1Click(NoticeDialogFragment dialog);
+        public void onDialogBtn2Click(NoticeDialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener listener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException("Caller must implement NoticeDialogListener");
+        }
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btn1=view.findViewById(R.id.btn1);
+        btn2=view.findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getActivity().getApplicationContext(), "Bye", Toast.LENGTH_SHORT).show();
+                dismiss();
+                listener.onDialogBtn1Click(NoticeDialogFragment.this);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast.makeText(getActivity().getApplicationContext(), "Bye", Toast.LENGTH_SHORT).show();
+                dismiss();
+                listener.onDialogBtn2Click(NoticeDialogFragment.this);
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.customview,container,false);
+    }
+
+
+}
